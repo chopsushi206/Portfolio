9591b97f185498504bfd8db0567f0ffe9e23a510
LiHua Anderson <lihua.anderson@gmail.com>
2021-02-09 15:56:42 -0800

    removed superflous line of code from script.

--- assets/script/nav.js
+++ assets/script/nav.js
@@ -1,7 +1,6 @@
 const navSlide = () => {
     const icon = document.querySelector('.icon');
     const nav = document.querySelector('.links');
-    const links = document.querySelectorAll('.links li');
 
     icon.addEventListener('click', () => {
         nav.classList.toggle('icon-active');

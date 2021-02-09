05308d5e8cc8df5edc68d3b70ac6630fe58714af
LiHua Anderson <lihua.anderson@gmail.com>
2021-02-08 23:23:58 -0800

    Refractored a lot of the index.html code as well as the CSS. also changed the responsive menu to a small javascript making it more semantic and cleaner to look at.

---
+++ assets/script/nav.js
@@ -0,0 +1,11 @@
+const navSlide = () => {
+    const icon = document.querySelector('.icon');
+    const nav = document.querySelector('.links');
+    const links = document.querySelectorAll('.links li');
+
+    icon.addEventListener('click', () => {
+        nav.classList.toggle('icon-active');
+    });
+}
+
+navSlide();

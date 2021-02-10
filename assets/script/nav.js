a0e49a62d06f09f4e4af858a7d5f38935749f42f
LiHua Anderson <lihua.anderson@gmail.com>
2021-02-09 23:25:44 -0800

    removed float tag from developer photo and used a flex box in the CSS instead.

--- assets/script/nav.js
+++ assets/script/nav.js
@@ -1,10 +1,16 @@
 const navSlide = () => {
     const icon = document.querySelector('.icon');
     const nav = document.querySelector('.links');
+    const links = document.querySelectorAll('.links li');
 
     icon.addEventListener('click', () => {
         nav.classList.toggle('icon-active');
     });
+  links.forEach(link => {
+      link.addEventListener('click', () => {
+          nav.classList.toggle('icon-active');
+      })
+  });
 }
 
 navSlide();
